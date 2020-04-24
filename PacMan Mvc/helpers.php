d0efed6c923633d58918ada8589feb9ae0136547
Viezas <61420108+Viezas@users.noreply.github.com>
2020-04-24 16:46:06 +0200

    Add files via upload

--- PacMan Mvc/helpers.php
+++ PacMan Mvc/helpers.php
@@ -4,7 +4,7 @@
 function dbConnect()
 {
     try {
-        $db = new PDO('mysql:host=localhost:3306;dbname=dv19ngauv;charset=utf8', 'dv19ngauv', 'tqW1_2c0', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $db = new PDO('mysql:host=localhost; dbname=pacman; charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     } catch (Exception $exception)
     {
         die('Erreur : ' . $exception->getMessage());
